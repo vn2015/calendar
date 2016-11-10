b9e0dcccf8c62e3b3aab6a83040ca4b231a41cdc
Nedogarko Vladimir <nnedogarko@yandex.ru>
2016-11-10 12:28:49 +0200

    Default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create({ :username => "Admin",  :email => "admin@admin.com", :password =>"1q2w3e4r", :password_confirmation =>"1q2w3e4r"})
