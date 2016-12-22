85097f7c23acd1f95f12ea79a8a0be90f3d06423
Nedogarko Vladimir <nnedogarko@yandex.ru>
2016-12-22 12:27:09 +0200

    Cnahge date format

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :meetingtypes
   #resources :clients
   resources :programs
-  devise_for :users #, :except => 'create'
+  devise_for :users , :except => 'create_user'
   resources :users #, :except => 'create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
